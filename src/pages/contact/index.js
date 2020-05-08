435dee6c18d5315139a9e6a8630a591886137050
sidskaka <47903172+sidskaka@users.noreply.github.com>
2020-05-08 21:03:38 +0200

    contact form conf

---
+++ src/pages/contact/index.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+
+class Index extends Component {
+
+   render() {
+      return(
+         <div>
+            <form method="POST" name="Contact">
+               <label for="fname"> Nom </label>
+               <br/>
+               <input type="text" id="fname" name="firstname" />
+               <br/>
+               <br/>
+               <label for="lname"> Nom </label>
+               <br/>
+               <input type="text" id="lname" name="lastname" />
+               <br/>
+               <br/>
+               <label for="email"> Email </label>
+               <br/>
+               <input type="text" id="email" name="email" />
+               <br/>
+               <br/>
+               <label for="message"> Message </label>
+               <br/>
+               <textarea name="message" id="message" name="lastname"></textarea>
+               <br/>
+               <br/>
+               <input type="submit" value="submit" />
+            </form>
+         </div>
+      )
+   }
+}
+
+export default Index
