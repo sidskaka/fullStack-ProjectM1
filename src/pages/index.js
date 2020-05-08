1dcb1f6e0e423595a26bc3620f55d79905bb2929
sidskaka <47903172+sidskaka@users.noreply.github.com>
2020-05-09 00:53:26 +0200

    contact form conf

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,9 +27,24 @@ const IndexPage = () => (
     data-netlify="true" 
     data-netlify-honeypot="bot-field">
       
-        <input type="text" name="name" placeholder="Your name" />
+        {/* <input type="text" name="name" placeholder="Your name" /> */}
+        <div className="form-group">
+          <label htmlFor="email">
+            Email
+          </label>
+          <input type="email" name="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="firstName">
+            Nom
+          </label>
+          <input type="text" name="firstName" />
+
+        </div>
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="submit" value="envoyer" />
  
-      <button>SEND</button>
+      {/* <button>SEND</button> */}
     </form>
   </Layout>
 )
