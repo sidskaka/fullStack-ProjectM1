1c3b06138d9462669c3f968dfc03a67d98549aae
sidskaka <47903172+sidskaka@users.noreply.github.com>
2020-05-08 18:13:32 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `products`,
-        path: `${__dirname}/products`,
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
