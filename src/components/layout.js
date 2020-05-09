e9257303db5d3ae9c37a217c47295314c1893819
sidskaka <47903172+sidskaka@users.noreply.github.com>
2020-05-09 19:28:02 +0200

    last modifs

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,20 +39,13 @@ const Layout = ({ children }) => {
             <nav>
                 <div className="first-div">
                     <ul>
-                        {/* <li><a href="/">Accueil</a></li>
+                        <li><a href="/">Accueil</a></li>
                         <li><a href="/contacts/contactForm">Contact</a></li>
                         <li><a href="/stripe/products-stripe-checkout">Checkout Stripe</a></li>
                         <li><a href="/categories/">Catégorie</a></li>
                         <li><a href="/products/products">Produit</a></li>
                         <li><a href="#">Mon compte</a></li>
-                        <li><a href="/blogs/blog">Blog</a></li> */}
-                        <li><a href="/">Accueil</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Checkout Stripe</a></li>
-                        <li><a href="/categories/">Catégorie</a></li>
-                        <li><a href="/products/products">Produit</a></li>
-                        <li><a href="#">Mon compte</a></li>
-                        <li><a href="#">Blog</a></li>
+                        <li><a href="/blogs/blog">Blog</a></li>
                     </ul>
                 </div>
                 <div className="second-div">
