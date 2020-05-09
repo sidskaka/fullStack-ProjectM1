db2b6c2e1b7735097ff2808b422c09118465b281
sidskaka <47903172+sidskaka@users.noreply.github.com>
2020-05-09 19:38:10 +0200

    last modifs

---
+++ src/pages/stripe/success.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class success extends Component {
+   constructor(props) {
+      super(props)
+
+      this.state = {
+             
+      }
+   }
+
+   render() {
+      return (
+         <div>
+            success
+         </div>
+      )
+   }
+}
+
+export default success
