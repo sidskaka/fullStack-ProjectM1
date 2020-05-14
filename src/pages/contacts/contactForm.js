fd996cfcde6eace47af1b4f7a0ad2f6f4b7e667b
sidskaka <47903172+sidskaka@users.noreply.github.com>
2020-05-14 14:29:23 +0200

    modif du 14 mai 2020

--- src/pages/contacts/contactForm.js
+++ src/pages/contacts/contactForm.js
@@ -41,19 +41,28 @@ class ContactForm extends Component {
 
                   <div className="row">
                   <div className="col-md-8">
-                        <form 
-                           method="post" 
-                           name="contact" 
-                           data-netlify="true" 
-                           data-netlify-honeypot="bot-field">
-                           <div className="form-group">
-                              <input name="firstName" type="text" placeholder="Name..." />
-                           </div><br />
-                           <input className="form-control" name="phone" type="number" placeholder="(+33)" /><br />
-                           <input className="form-control" name="email" type="email" placeholder="E-mail..." /><br />
-                           <textarea className="form-control" name="text" placeholder="Dite nous comment vous aidez ?" style={{height: '150px'}}></textarea><br />
-                           <input className="btn btn-primary" type="submit" value="Envoyer" /><br /><br />
-                        </form>
+                     <form 
+                        method="post" 
+                        name="contact" 
+                        data-netlify="true" 
+                        data-netlify-honeypot="bot-field">
+                        <div className="form-group">
+                           <label htmlFor="firstName">
+                              Nom
+                           </label>
+                           <input type="text" name="firstName" />
+                        </div><br />
+                        <input className="form-control" name="phone" type="number" placeholder="(+33)" /><br />
+                        {/* <input className="form-control" name="email" type="email" placeholder="E-mail..." /><br /> */}
+                        <div className="form-group">
+                           <label htmlFor="email">
+                              Email
+                           </label>
+                           <input type="email" name="email" />
+                        </div>
+                        <textarea className="form-control" name="text" placeholder="Dite nous comment vous aidez ?" style={{height: '150px'}}></textarea><br />
+                        <input className="btn btn-primary" type="submit" value="Envoyer" /><br /><br />
+                     </form>
                   </div>
                   <div class="col-md-4">
                      <b>Customer service:</b> <br />
