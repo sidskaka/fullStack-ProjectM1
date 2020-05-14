65dfcd3705eba510c282c5fa35e07229575ab898
sidskaka <47903172+sidskaka@users.noreply.github.com>
2020-05-14 14:15:47 +0200

    modif du 14 mai 2020

--- src/pages/contacts/contactForm.js
+++ src/pages/contacts/contactForm.js
@@ -34,6 +34,42 @@ class ContactForm extends Component {
                   <input type="submit" value="envoyer" />
             
                </form>
+
+               <div className="container">
+
+                  <h3 className="text-center">Contactez-nous facilement</h3><br />
+
+                  <div className="row">
+                  <div className="col-md-8">
+                        <form 
+                           method="post" 
+                           name="contact" 
+                           data-netlify="true" 
+                           data-netlify-honeypot="bot-field">
+                           <input className="form-control" name="firstName" type="text" placeholder="Name..." /><br />
+                           <input className="form-control" name="phone" type="number" placeholder="(+33)" /><br />
+                           <input className="form-control" name="email" type="email" placeholder="E-mail..." /><br />
+                           <textarea className="form-control" name="text" placeholder="Dite nous comment vous aidez ?" style={{height: '150px'}}></textarea><br />
+                           <input className="btn btn-primary" type="submit" value="Envoyer" /><br /><br />
+                        </form>
+                  </div>
+                  <div class="col-md-4">
+                     <b>Customer service:</b> <br />
+                     Phone: +1 129 209 291<br />
+                     E-mail: <a href="mailto:support@mysite.com">support@mysite.com</a><br />
+                     <br /><br />
+                     <b>Headquarter:</b><br />
+                     Company Inc, <br />
+                     Las vegas street 201<br />
+                     55001 Nevada, USA<br />
+                     Phone: +1 145 000 101<br />
+                     <a href="mailto:usa@mysite.com">usa@mysite.com</a><br />
+
+
+                  </div>
+                  </div>
+
+               </div>
             </div>
          </Layout>
       )
